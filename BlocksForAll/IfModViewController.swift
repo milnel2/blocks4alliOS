3a68e9fae4affd59e4cc3b015ea2721bb323a054
Nana Amoah <namoah@macalester.edu>
2019-07-08 11:50:50 -0500

    Fixed error when button in If View Mod is clicked.

--- BlocksForAll/IfModViewController.swift
+++ BlocksForAll/IfModViewController.swift
@@ -14,46 +14,15 @@ class IfModViewController: UIViewController{
     
     var modifierBlockIndexSender: Int?
     var booleanSelected: String = "hear_voice"
+
     
     @IBAction func hearVoiceBoolean(_ sender: Any) {
         booleanSelected = "hear_voice"
-        
-        //Border when button is selected
-        if let button = sender as? UIButton {
-            if button.isSelected {
-                button.isSelected = false
-                button.layer.borderWidth = 0
-            } else {
-                button.isSelected = true
-                button.layer.borderWidth = 2
-                button.layer.borderColor = UIColor.black.cgColor
-            }
-        }
     }
     
-
-    
-    @IBAction func obstacleSensed(_ sender: Any) {
+    @IBAction func senseObstacleBoolean(_ sender: Any) {
         booleanSelected = "obstacle_sensed"
-        
-        //Border when button is selected
-        if let button = sender as? UIButton {
-            if button.isSelected {
-                button.isSelected = false
-                button.layer.borderWidth = 0
-            } else {
-                button.isSelected = true
-                button.layer.borderWidth = 2
-                button.layer.borderColor = UIColor.black.cgColor
-            }
-        }
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
-        if segue.destination is BlocksViewController{
-            blocksStack[modifierBlockIndexSender!].addedBlocks[0].attributes["booleanSelected"] = booleanSelected
-            
-        }
-    }
     
 }
