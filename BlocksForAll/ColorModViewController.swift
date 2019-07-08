e652800b29a2712e868758453c18e0f2ddc20ba1
Nana Amoah <namoah@macalester.edu>
2019-07-08 12:40:39 -0500

    Added borders to the buttons in the Color Mod View Controller that show when the button is selected.

--- BlocksForAll/ColorModViewController.swift
+++ BlocksForAll/ColorModViewController.swift
@@ -17,27 +17,126 @@ class ColorModViewController: UIViewController{
     
     @IBAction func blackButtonPressed(_ sender: UIButton) {
         colorSelected = "black"
+        //Border when button is selected
+        if let button = sender as? UIButton {
+            if button.isSelected {
+                button.isSelected = false
+                button.layer.borderWidth = 0
+            } else {
+                button.isSelected = true
+                button.layer.borderWidth = 5
+                button.layer.borderColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+            }
+        }
     }
+    
     @IBAction func redButtonPressed(_ sender: UIButton) {
         colorSelected = "red"
+        //Border when button is selected
+        if let button = sender as? UIButton {
+            if button.isSelected {
+                button.isSelected = false
+                button.layer.borderWidth = 0
+            } else {
+                button.isSelected = true
+                button.layer.borderWidth = 5
+                button.layer.borderColor = #colorLiteral(red: 0, green: 1, blue: 0.2436479628, alpha: 1)
+            }
+        }
     }
+    
     @IBAction func orangeButtonPressed(_ sender: UIButton) {
         colorSelected = "orange"
+        //Border when button is selected
+        if let button = sender as? UIButton {
+            if button.isSelected {
+                button.isSelected = false
+                button.layer.borderWidth = 0
+            } else {
+                button.isSelected = true
+                button.layer.borderWidth = 5
+                button.layer.borderColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+            }
+        }
     }
+    
     @IBAction func yellowButtonPressed(_ sender: UIButton) {
         colorSelected = "yellow"
+        //Border when button is selected
+        if let button = sender as? UIButton {
+            if button.isSelected {
+                button.isSelected = false
+                button.layer.borderWidth = 0
+            } else {
+                button.isSelected = true
+                button.layer.borderWidth = 5
+                button.layer.borderColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+            }
+        }
     }
+    
     @IBAction func greenButtonPressed(_ sender: UIButton) {
         colorSelected = "green"
+        //Border when button is selected
+        if let button = sender as? UIButton {
+            if button.isSelected {
+                button.isSelected = false
+                button.layer.borderWidth = 0
+            } else {
+                button.isSelected = true
+                button.layer.borderWidth = 5
+                button.layer.borderColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+            }
+        }
     }
+    
     @IBAction func blueButtonPressed(_ sender: UIButton) {
         colorSelected = "blue"
+        //Border when button is selected
+        if let button = sender as? UIButton {
+            if button.isSelected {
+                button.isSelected = false
+                button.layer.borderWidth = 0
+            } else {
+                button.isSelected = true
+                button.layer.borderWidth = 5
+                button.layer.borderColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+            }
+        }
     }
+    
     @IBAction func purpleButtonPressed(_ sender: UIButton) {
         colorSelected = "purple"
+        //Border when button is selected
+        if let button = sender as? UIButton {
+            if button.isSelected {
+                button.isSelected = false
+                button.layer.borderWidth = 0
+            } else {
+                button.isSelected = true
+                button.layer.borderWidth = 5
+                button.layer.borderColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+            }
+        }
     }
+    
+    
     @IBAction func whiteButtonPressed(_ sender: UIButton) {
         colorSelected = "white"
+        //Border when button is selected
+        
+        if let button = sender as? UIButton {
+            if button.isSelected {
+                button.isSelected = false
+                button.layer.borderWidth = 0
+
+                
+            } else {
+                button.isSelected = true
+                button.layer.borderWidth = 5
+                button.layer.borderColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+            }
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?){
