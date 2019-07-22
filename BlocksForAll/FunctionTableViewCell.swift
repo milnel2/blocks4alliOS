2c8a46941efdc29a4c4a29349213a7173341d2af
Mariella Page <mpage@macalester.edu>
2019-07-22 11:47:34 -0500

    updated segeues
    updated segues

--- BlocksForAll/FunctionTableViewCell.swift
+++ BlocksForAll/FunctionTableViewCell.swift
@@ -32,22 +32,23 @@ class FunctionTableViewCell: UITableViewCell {
         return button
     }()
     
-    let nameLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Sample Function"
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
+    let nameButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sample Function", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
     }()
 
     func setUpViews(){
-        addSubview(nameLabel)
+        addSubview(nameButton)
         addSubview(deleteButton)
         
         deleteButton.addTarget(self, action: #selector(deleteAction(sender:)), for: .touchUpInside)
+        nameButton.addTarget(self, action: #selector(editFunction(sender:)), for: .touchUpInside)
         
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]-8-[v1(80)]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel, "v1": deleteButton]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]-8-[v1(80)]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameButton, "v1": deleteButton]))
         
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameButton]))
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": deleteButton]))
     }
     
@@ -55,6 +56,13 @@ class FunctionTableViewCell: UITableViewCell {
         print("tapped")
         functionTableViewController?.deleteCell(cell: self)
     }
+    
+    @objc func editFunction(sender: UIButton){
+        print("tapped")
+        functionTableViewController?.blockModifier(sender: nil)
+    }
+    
+
     
     override func awakeFromNib() {
         super.awakeFromNib()
