685f66ef103bd9fa0fae496b15bdf1b7b16678d6
lucyrubin <lucyjr111@gmail.com>
2023-06-08 11:49:06 -0500

    changed to segmented control for choose icons in settings

--- BlocksForAll/SettingsViewController.swift
+++ BlocksForAll/SettingsViewController.swift
@@ -10,58 +10,23 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
-    @IBOutlet weak var showText: UIButton!
-    
-    @IBOutlet weak var showIcons: UIButton!
+    @IBOutlet weak var showIconsOrText: UISegmentedControl!
     
     // from Paul Hegarty, lectures 13 and 14
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
-        showIcons.titleLabel?.adjustsFontForContentSizeCategory = true
-        showText.titleLabel?.adjustsFontForContentSizeCategory = true
+        // select the current setitng
+        // value is 0 to show icons and 1 to show text
+        showIconsOrText.selectedSegmentIndex = defaults.value(forKey: "showText") as! Int
+    }
+    @IBAction func showIconsOrTextSelected(_ sender: UISegmentedControl, forEvent event: UIEvent) {
         
-        if (defaults.integer(forKey: "showText") == 0) {
-            showIcons.layer.borderWidth = 10
-            showIcons.layer.borderColor = #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
-            showIcons.accessibilityValue = "selected"
-            showText.layer.borderColor = #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
-            showText.accessibilityValue = "unselected"
-            showText.accessibilityHint = "double tap to select"
-        } else {
-            showText.layer.borderWidth = 10
-            showText.layer.borderColor = #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
-            showText.accessibilityValue = "selected"
-            showIcons.layer.borderColor = #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
-            showIcons.accessibilityValue = "unselected"
-            showIcons.accessibilityHint = "double tap to select"
-        }
-
-        // Do any additional setup after loading the view.
-    }
-    
-    @IBAction func showIconsSelected(_ sender: Any) {
-        defaults.setValue(0, forKey: "showText")
-        showText.layer.borderWidth = 0
-        showIcons.layer.borderWidth = 10
-        showIcons.accessibilityValue = "selected"
-        showText.accessibilityValue = "unselected"
-        showText.accessibilityHint = "double tap to select"
+        // set value to 0 to show icons and 1 to show text
+        defaults.setValue(sender.selectedSegmentIndex, forKey: "showText")
         defaults.synchronize()
     }
     
-    
-    
-    @IBAction func showTextSelected(_ sender: Any) {
-        defaults.setValue(1, forKey: "showText")
-        showIcons.layer.borderWidth = 0
-        showText.layer.borderWidth = 10
-        showText.accessibilityValue = "selected"
-        showIcons.accessibilityValue = "unselected"
-        showIcons.accessibilityHint = "double tap to select"
-        defaults.synchronize()
-    }
-
     /*
     // MARK: - Navigation
 
