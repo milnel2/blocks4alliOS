5186046326901070d29af56aef19959ac8136d46
Lauren Milne <lauren.r.milne@gmail.com>
2023-06-13 11:19:44 -0500

    Update AnimalModViewController.swift
    Changed so VO reads them in order

--- BlocksForAll/ModifierViewControllers/AnimalModViewController.swift
+++ BlocksForAll/ModifierViewControllers/AnimalModViewController.swift
@@ -26,7 +26,7 @@ class AnimalModViewController: UIViewController{
     override func viewDidLoad() {
         
         //reroute VO Order to be more intuitive
-        animalModView.accessibilityElements = [animalTitle!,buttons!, back!]
+        //animalModView.accessibilityElements = [animalTitle!,buttons!, back!]
         
         // default: Cat or preserve last selection
         let previousAnimal: String = functionsDict[currentWorkspace]![modifierBlockIndexSender!].addedBlocks[0].attributes["animalNoise"] ?? "cat"
@@ -38,6 +38,9 @@ class AnimalModViewController: UIViewController{
             if animalSelected == button.accessibilityIdentifier {
                 button.layer.borderWidth = 10
                 button.layer.borderColor = #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
+                button.isSelected = true
+            }else{
+                button.isSelected = false
             }
             createVoiceControlLabels(button: button)
         }
@@ -48,12 +51,14 @@ class AnimalModViewController: UIViewController{
         //Deselects all buttons but currently selected one (only one can be selected at a time)
         self.buttons.forEach { (button) in
             button.layer.borderWidth = 0
+            button.isSelected = false
                 }
         //Selects pressed button
         sender.layer.borderWidth = 10
         sender.layer.borderColor = #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
         if let buttonID = sender.accessibilityIdentifier {
             animalSelected = buttonID
+            sender.isSelected = true
         }
     }
     
