2c690336eb3a263036123a4c7d506af3d92cd6d5
Lauren Milne <lauren.r.milne@gmail.com>
2023-06-15 11:31:39 -0500

    added saving to when the application terminates

--- BlocksForAll/AppDelegate.swift
+++ BlocksForAll/AppDelegate.swift
@@ -56,6 +56,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        save()
     }
     
     // from Paul Hegarty, lectures 13 and 14
