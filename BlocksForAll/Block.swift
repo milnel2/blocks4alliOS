008457ea5402cc9fa25de45bdb99762c35b0eb40
Miri Leonard <123122852+MiriRL@users.noreply.github.com>
2023-06-30 12:16:12 -0500

    Block file stylized

--- BlocksForAll/Block.swift
+++ BlocksForAll/Block.swift
@@ -4,26 +4,31 @@
 //  Created by Lauren Milne on 2/28/17.
 //  Copyright © 2017 Lauren Milne. All rights reserved.
 //
+//
+// The code below came from:
+//      https://stackoverflow.com/questions/50928153/make-uicolor-codable
 
-// The code below came from https://stackoverflow.com/questions/50928153/make-uicolor-codable
-// The function below makes a struct Color and creates a uiColor from it while conforming to the codable forms that swift allows for encoding and decoding
 import UIKit
-
+//MARK: - Codable Color Class
+/// Makes a struct Color and creates a uiColor from it while conforming to the codable forms that swift allows for encoding and decoding.
 struct Color : Codable {
-    // struct used to make a codable UIColor
+    
+    // Struct used to make a codable UIColor.
     var red : CGFloat = 0.0, green: CGFloat = 0.0, blue: CGFloat = 0.0, alpha: CGFloat = 0.0
-    // create variables used to construct a UIColor
+    
+    // Creates variables used to construct a UIColor.
     var uiColor : UIColor {
-        // takes the Color object uses variables above, initialized below to create and return a UIColor object
+        // Takes the Color object uses variables above, initialized below to create and return a UIColor object.
         return UIColor(red: red, green: green, blue: blue, alpha: alpha)
     }
+    
     init(uiColor : UIColor) {
-        //initializes a color
+        // Initializes a color.
         uiColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
     }
 }
 
-//Below is the actual encoding and decoding of the uiColor
+/// This is the actual encoding and decoding of the uiColor.
 struct Task: Codable {
     
     private enum CodingKeys: String, CodingKey { case content, deadline, color }
@@ -52,52 +57,60 @@ struct Task: Codable {
         try container.encode(Color(uiColor: color), forKey: .color)
     }
 }
-//above code is from https://stackoverflow.com/questions/50928153/make-uicolor-codable
+// Above code is from https://stackoverflow.com/questions/50928153/make-uicolor-codable
 
+//MARK: - Block Class
+/// Block model that has all the properties describing the block
 class Block: Codable {
-    /*Block model that has all the properties describing the block*/
     
-    //MARK: - Properties
+    //MARK: Properties
     
     var name: String
-    var color: Color //Color struct rather than UIColor so as to be codable
-    var double: Bool //true if needs both beginning and end block like repeat, if
-    var counterpart: [Block] = [] //start and end block counterpart for for etc.
+    var color: Color  // Color struct rather than UIColor so as to be codable.
+    var imageName: String?  // The block's image in assets.
     
-    var imageName: String?
+    var double: Bool  // True if the block needs both beginning and end blocks, like repeat and if blocks.
+    var counterpart: [Block] = []  // Start and end block counterpart for For, etc.
     
-    //For control blocks (if and repeat blocks)
-    var addedBlocks: [Block] = [] //blocks that modify the current block (e.g. two times for a repeat block)
-    var type: String = "Operation" //types: Operations, Booleans, Numbers
-    var acceptedTypes: [String] = [] //list of types that can be added to current block (e.g. numbers for a repeat block)
+    //For control blocks (if and repeat blocks).
+    var addedBlocks: [Block] = []  // Blocks that modify the current block (e.g. two times for a repeat block).
+    var type: String = "Operation"  // Types: Operations, Booleans, Numbers
+    var acceptedTypes: [String] = []  // List of types that can be added to current block (e.g. numbers for a repeat block).
     
-    var attributes = [String:String]() //dictionary that holds attibute values for block (e.g. distance and speed)
-    var isModifiable: Bool? //true if it has a modifier block
+    var attributes = [String : String]()  // Dictionary that holds attribute values for block (e.g. distance and speed).
+    var isModifiable: Bool?  // True if it has a modifier block.
+    var isInToolBox: Bool?  // True if this block is being shown in the toolbox. False for blocks in the workspace.
     
-    var isInToolBox: Bool? // true if this block is being shown in the toolbox. False for blocks in the workspace
     
-    
-    //MARK: - json variable
-    // From Paul, create a variable for creating the json encoded data, using the self data of the block in question
-    var jsonVar: Data? {
+    //MARK: - JSON Variable
+    // From Paul: Create a variable for creating the json encoded data, using the self data of the block in question.
+    var jsonVar: Data?
+    {
+        // Gets counterpart to be re-added later, then sets the counterpart to nil so it's codable
         let blocksCounterpart = self.counterpart
         self.counterpart = []
-        // TODO: needs to have altered to work with IfElse blocks properly, right now their counterparts are not stored after a save, need to set each item in the array of counterparts counterparts to nil so it only saves one level and not recursively
-        // gets counterpart to be re-added later, then sets the counterpart to nil so its codable
-        let jsonString = try? JSONEncoder().encode(self)
-        // try to encode self to a JSON object
-        self.counterpart = blocksCounterpart
-        // adds the counterpart back to the block
+        
+        let jsonString = try? JSONEncoder().encode(self)  // Tries to encode self to a JSON object
+        self.counterpart = blocksCounterpart  // Adds the counterpart back to the block
+        
         return jsonString
     }
     
     
     //MARK: - Initialization
-
-    init?(name: String, color: Color, double: Bool, imageName: String? = nil, addedBlocks: [Block] = [], type: String = "Operation", acceptedTypes: [String] = [], isModifiable: Bool = false){
+    /// If isInToolbox is not initialized, it will be set to false.
+    init?(name: String,
+          color: Color,
+          double: Bool,
+          imageName: String? = nil,
+          addedBlocks: [Block] = [],
+          type: String = "Operation",
+          acceptedTypes: [String] = [],
+          isModifiable: Bool = false)
+    {
         
         //TODO: check that color is initialized as well
-        if name.isEmpty {
+        if name.isEmpty{
             return nil
         }
         
@@ -109,11 +122,18 @@ class Block: Codable {
         self.type = type
         self.acceptedTypes = acceptedTypes
         self.isModifiable = isModifiable
-        self.isInToolBox = false
-        
+        self.isInToolBox = false  // Assumes the block is not in the toolbox on initialization.
     }
-    
-    init?(name: String, color: Color, double: Bool, imageName: String? = nil, addedBlocks: [Block] = [], type: String = "Operation", acceptedTypes: [String] = [], isModifiable: Bool = false, isInToolBox: Bool){
+    init?(name: String,
+          color: Color,
+          double: Bool,
+          imageName: String? = nil,
+          addedBlocks: [Block] = [],
+          type: String = "Operation",
+          acceptedTypes: [String] = [],
+          isModifiable: Bool = false,
+          isInToolBox: Bool)
+    {
         
         //TODO: check that color is initialized as well
         if name.isEmpty {
@@ -129,24 +149,30 @@ class Block: Codable {
         self.acceptedTypes = acceptedTypes
         self.isModifiable = isModifiable
         self.isInToolBox = isInToolBox
-        
     }
     
-    func addImage(_ imageName: String){
+    //MARK: - Public Functions
+    /// Adds an image to the block using a file name.
+    func addImage(_ imageName: String)
+    {
         self.imageName = imageName
     }
     
-    func addAttributes(key: String, value: String){
+    func addAttributes(key: String, value: String)
+    {
         self.attributes[key] = value
     }
     
-    func copy() -> Block{
-        /* Used when selecting a block from the toolbox and copying into workspace*/
+    /// Used when selecting a block from the toolbox and copying into workspace.
+    func copy() -> Block
+    {
         let newBlock = Block.init(name: self.name, color: self.color, double: self.double, imageName: self.imageName, addedBlocks: self.addedBlocks, type: self.type, acceptedTypes: self.acceptedTypes, isModifiable: self.isModifiable!)
+        
         return newBlock!
     }
     
-    func getDocumentsDirectory() -> URL{
+    func getDocumentsDirectory() -> URL
+    {
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         return paths[0]
     }
