50e646013907de513306de6d58cfbbc263df928b
lucyrubin <lucyjr111@gmail.com>
2023-06-30 10:46:54 -0500

    Reformatted angle modifier code file and fixed a bug in selected block view controller

--- BlocksForAll/ModifierViewControllers/AngleModViewController.swift
+++ BlocksForAll/ModifierViewControllers/AngleModViewController.swift
@@ -11,52 +11,58 @@ import UIKit
 
 class AngleModViewController: UIViewController {
     /* Custom view controller for the Angle modifier scene */
+    
+    // Angle variables
     var angle: Double = 90
     var modifierBlockIndexSender: Int?
-    let interval: Float = 15
     var roundedAngle: Float = 90
+    let interval: Float = 15
     
+    // View controller elements
     @IBOutlet weak var angleSlider: UISlider!
     @IBOutlet weak var angleDisplayed: UILabel!
-    
     @IBOutlet weak var back: UIButton!
-    
     @IBOutlet var turnView: UIView!
-
     @IBOutlet weak var angleTitle: UILabel!
     
     override func viewDidLoad() {
-        
         // default angle: 90 or preserve last selection
-        //turnView.accessibilityElements = [angleTitle!, angleDisplayed!, angleSlider!, back!]
         let previousAngleString: String = functionsDict[currentWorkspace]![modifierBlockIndexSender!].addedBlocks[0].attributes["angle"] ?? "90"
         let previousAngle = Int(previousAngleString)
+        
+        // Update the values on the screen
         angleDisplayed.text = "\(previousAngle ?? 90)"
         angleSlider.setValue(Float(previousAngle!), animated: false)
         angleSlider.accessibilityValue = "\(previousAngle ?? 90) degrees"
-        
-        // preserves previously selected value
-        roundedAngle = Float(Double(previousAngle!))
         angleDisplayed.accessibilityValue = "Current angle is \(Int(angle))degrees"
         
+        roundedAngle = Float(Double(previousAngle!))
+        
+        // Accessibility
         back.titleLabel?.adjustsFontForContentSizeCategory = true
+        turnView.accessibilityElements = [back!, angleTitle!, angleDisplayed!, angleSlider!]
     }
     
     /// When angle slider moved, get rounded value and convert to degrees 
     @IBAction func angleSliderChanged(_ sender: UISlider) {
-        let roundingNumber: Float = (interval/2.0)
+        // Calculate rounded value
+        let roundingNumber: Float = (interval / 2.0)
         angle = Double(sender.value)
-        roundedAngle = (interval*floorf(((sender.value+roundingNumber)/interval)))
-        sender.accessibilityValue = "\(Int(roundedAngle)) degrees"
+        roundedAngle = (interval*floorf(((sender.value + roundingNumber) / interval)))
+        
+        // Update the screen
         sender.setValue(roundedAngle, animated:false)
         angleDisplayed.text = "\(Int(roundedAngle))"
         
+        // Accessibility
+        sender.accessibilityValue = "\(Int(roundedAngle)) degrees"
         angleDisplayed.accessibilityValue = "Current angle is \(Int(roundedAngle)) degrees"
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
-        if segue.destination is BlocksViewController{
-            print("\(roundedAngle)")
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.destination is BlocksViewController {
+            
+            print("Set angle to \(roundedAngle) degrees")
             functionsDict[currentWorkspace]![modifierBlockIndexSender!].addedBlocks[0].attributes["angle"] = "\(Int(roundedAngle))"
         }
     }
