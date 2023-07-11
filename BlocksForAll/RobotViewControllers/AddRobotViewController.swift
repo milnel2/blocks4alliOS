8cdc00eec03a71234b7d7b204562187a43879a3f
Lauren Milne <lauren.r.milne@gmail.com>
2023-07-11 16:04:13 -0500

    Fix add robot back button
    Add robot back button returns to either workspace or settings menu

---
+++ BlocksForAll/RobotViewControllers/AddRobotViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AddRobotViewController.swift
+//  BlocksForAll
+//
+//  Created by lmilne on 7/11/23.
+//  Copyright © 2023 Blocks4All. All rights reserved.
+//
+
+import UIKit
+
+class AddRobotViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
