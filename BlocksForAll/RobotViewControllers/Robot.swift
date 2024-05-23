57cf9286e645aef040f2844d203c98cdce61c588
lucyrubin <lucyjr111@gmail.com>
2024-05-23 14:29:54 -0500

    Added comments to Robot class

--- BlocksForAll/RobotViewControllers/Robot.swift
+++ BlocksForAll/RobotViewControllers/Robot.swift
@@ -8,24 +8,20 @@
 
 import UIKit
 import CoreBluetooth
-
+/// Class to represent the Core Bluetooth Peripheral Object for each robot that is controlled by Blocks4All
 class Robot: Equatable {
     
     var peripheral: CBPeripheral // peripheral object used for Core Bluetooth
-    
     
     // Sensors currently in use by Blocks4All
         // Sound
     var soundLevel: Int // value from 0 to 255 of how much sound is heard
     var soundDirection: Int //TODO: description
-    
         // Object detection
     var leftDistanceSensor: Int // value from 0 to 255 of how close an object is (255 is closest, 0 is farthest)
     var rightDistanceSensor: Int // value from 0 to 255 of how close an object is (255 is closest, 0 is farthest)
     
-    
-    
-    // Sensors not in use // TODO: organise these by category
+    // Sensors not in use
         // Buttons
     var button0: Bool // Middle white button on top of head
     var button1: Bool // Bottom left orange button on top of head
@@ -54,7 +50,6 @@ class Robot: Equatable {
     var zRotationAcceleration: Int
     
     
-    
     static func == (lhs: Robot, rhs: Robot) -> Bool {
         return lhs.peripheral == rhs.peripheral
     }
@@ -66,6 +61,8 @@ class Robot: Equatable {
         // Sensors currently being used by Blocks4All
         self.soundLevel = 0
         self.soundDirection = 0
+        self.leftDistanceSensor = 0
+        self.rightDistanceSensor = 0
         
         // Sensors not currently being used
         self.button0 = false
@@ -79,8 +76,6 @@ class Robot: Equatable {
         self.leftSensorSeesDot = false
         self.rightSensorSeesDot = false
         self.dotWasSeen = false
-        self.leftDistanceSensor = 0
-        self.rightDistanceSensor = 0
         self.rearDistanceSensor = 0
         self.headX = 0
         self.headY = 0
@@ -94,7 +89,8 @@ class Robot: Equatable {
     }
     //TODO: documentation
     
-    func updateSensorData1(data: String) { 
+    /// Receive and parse through the first set of sensor data from the robot
+    func updateSensorData1(data: String) {
         // parse through data and update values
         // Code to interpret sensor data is from https://github.com/vdwel/RobotControl/tree/master
         
@@ -104,10 +100,11 @@ class Robot: Equatable {
         leftDistanceSensor = dataList[7]
         rightDistanceSensor = dataList[6]
         
-        
         // Sensors not currently in use
+            // Rear Distance Sensor
         rearDistanceSensor = dataList[8]
         
+            // Head X and Y
         let head = (dataList[0x12] << 8) + dataList[0x13]
         headX = head & 0b0000000111111111  // select last 9 bits
         headY = (head & 0b1111111000000000) >> 9  // select first 7 bits
@@ -123,10 +120,15 @@ class Robot: Equatable {
         headX = headX * 135 / 244 // convert to degrees
         headY = headY * 22 / 49 // convert to degrees
         
+            // Wheels
         leftWheel = (dataList[0x11] << 8) + dataList[0x10]
         rightWheel = (dataList[0x0F] << 8) + dataList[0x0E]
+        wheelDistance = (dataList[0x09] & 0b1111 << 12) + (dataList[0x0B] << 8) + dataList[0x0A]
+        if (wheelDistance > 0x7FFF) {
+            wheelDistance -= 0x10000
+        }
         
-        // Rotation Acceleration calculations
+            // Rotation Acceleration
         var z = (dataList[0x0D] << 8) + dataList[0x0C]
         var deltaZ = z - zRotationAcceleration
         
@@ -149,15 +151,10 @@ class Robot: Equatable {
         if (deltaYRotationAcceleration > 0x7FF) {
             deltaYRotationAcceleration -= 0x1000
         }
-        
-        wheelDistance = (dataList[0x09] & 0b1111 << 12) + (dataList[0x0B] << 8) + dataList[0x0A]
-        if (wheelDistance > 0x7FFF) {
-            wheelDistance -= 0x10000
-        }
     }
 
 
-    
+    /// Receive and parse through the second set of sensor data from the robot
     func updateSensorData2(data: String) {
         // parse through data and update values
         // Code to interpret sensor data is from https://github.com/vdwel/RobotControl/tree/master
@@ -166,6 +163,7 @@ class Robot: Equatable {
         
         
         // Sensors currently being used by Blocks4All
+            // Sound
         soundLevel =  dataList[0x07]
         
         if (dataList[0x0F] == 0x04) {
@@ -177,11 +175,13 @@ class Robot: Equatable {
         
         
         // Sensors not currently in use
+            // Buttons
         button0 = dataList[8] & 0b00010000 > 0
         button1 = dataList[8] & 0b00100000 > 0
         button2 = dataList[8] & 0b01000000 > 0
         button3 = dataList[8] & 0b10000000 > 0
         
+            // Tilt and lean
         tilt = ((dataList[0x04] & 0b11110000) << 4) + dataList[0x02] // TODO: remove tilt? I don't think we will ever really need it
         if (tilt > 0x7FF) {
             tilt -= 0x1000
@@ -192,13 +192,16 @@ class Robot: Equatable {
             lean -= 0x1000
         }
         
+            // Sound
         clap = (dataList[0x0B] & 0b00000001) == 1
  
+            // Acceleration
         zAcceleration = (dataList[0x05] << 4) + dataList[0x06]
         if (zAcceleration > 0x7FF) {
             zAcceleration -= 0x1000
         }
         
+            // Object detection
         if dataList[0x13] == 0x05 {
             leftSensorSeesDot = dataList[0x11] == 0xAA
             rightSensorSeesDot = dataList[0x12] == 0xAA
@@ -214,11 +217,12 @@ class Robot: Equatable {
     }
     
 
-    
+    /// Returns true if the soundlevel is above a certain threshold
     func canHearSound() -> Bool {
         return soundLevel > 50 // TODO: test value for accuracy
     }
     
+    /// Returns true if distance sensors are above a certain threshold to detect and object in front of the robot
     func isObstacleDetected() -> Bool {
         if leftDistanceSensor == 255 || rightDistanceSensor == 255 {
             return true
@@ -228,6 +232,7 @@ class Robot: Equatable {
         return averageSensorValue > 200
     }
     
+    /// Given a hex string, parses into an array of integers to use for sensor data
     func parseDataString(data: String) -> [Int] {
         // code to parse data string is from https://github.com/vdwel/RobotControl/tree/master
         let dataArray = Array(data)
@@ -243,8 +248,5 @@ class Robot: Equatable {
         
         return dataList
     }
-    
-
-
 }
 
