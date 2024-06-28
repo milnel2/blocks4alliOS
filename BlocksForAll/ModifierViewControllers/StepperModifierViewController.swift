9be3bd98b2ab7e99e31aefc3190fd3f72d6d943b
lucyrubin <lucyjr111@gmail.com>
2024-06-28 14:13:34 -0500

    Fixed colors on growth/shrink blocks

--- BlocksForAll/ModifierViewControllers/StepperModifierViewController.swift
+++ BlocksForAll/ModifierViewControllers/StepperModifierViewController.swift
@@ -23,8 +23,8 @@ class StepperModifierViewController: UIViewController {
     private let optionDictionary: [String:[String : String]] =
     ["Wait for Time" :  ["attributeName" : "wait", "min" : "1", "max" : "10", "unitIfSingular" : "second", "unitIfPlural" : "seconds", "increaseImage" : "orange_plus", "decreaseImage" : "orange_minus", "Default image" : "controlModifierBackground"],
      "Repeat" : ["attributeName" : "timesToRepeat", "min" : "2", "max" : "20", "unitIfSingular" : "time", "unitIfPlural" : "times", "increaseImage" : "orange_plus", "decreaseImage" : "orange_minus", "Default image" : "controlModifierBackground"],
-     "Grow Actor" : ["attributeName" : "sizeChangeAmount", "min" : "1", "max" : "5", "unitIfSingular" : "growth", "unitIfPlural" : "growth", "increaseImage" : "orange_plus", "decreaseImage" : "orange_minus", "Default image" : "controlModifierBackground"],
-     "Shrink Actor" : ["attributeName" : "sizeChangeAmount", "min" : "1", "max" : "5", "unitIfSingular" : "growth", "unitIfPlural" : "growth", "increaseImage" : "orange_plus", "decreaseImage" : "orange_minus", "Default image" : "controlModifierBackground"]]
+     "Grow Actor" : ["attributeName" : "sizeChangeAmount", "min" : "1", "max" : "5", "unitIfSingular" : "", "unitIfPlural" : "", "increaseImage" : "green_plus", "decreaseImage" : "green_minus", "Default image" : "driveModifierBackground"],
+     "Shrink Actor" : ["attributeName" : "sizeChangeAmount", "min" : "1", "max" : "5", "unitIfSingular" : "", "unitIfPlural" : "", "increaseImage" : "green_plus", "decreaseImage" : "green_minus", "Default image" : "driveModifierBackground"]]
     private var modifierValue = 2  // current value of the stepper
     private var attributeName = ""  // Used for accessing and saving data, taken from optionDictionary (ex. if optionType = "Wait for Time", attributeName is "wait"
     private var min = "0"  // minimum value of the stepper, taken from optionDictionary
