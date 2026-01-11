f45f5f8b7ccb1aa095c1ae576f5f16932d4df4b2
lucyrubin <lucyjr111@gmail.com>
2026-01-11 12:43:38 -0600

    Gallery view controller layout

---
+++ BlocksForAll/GalleryViewControllers/StartScreenGallery.swift
@@ -0,0 +1,10 @@
+//
+//  StartScreenGallery.swift
+//  BlocksForAll
+//
+//  Created by Lucy Rubin on 1/11/26.
+//  Copyright © 2026 Blocks4All. All rights reserved.
+//
+
+class StartScreenGallery: UIViewController {
+}
